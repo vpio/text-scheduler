03656f7cea95d9699cea40fc8ddef2980a5ff9bd
Pio Molina <vpio@comcast.net>
2019-09-05 12:28:38 -0500

    Added Message and Phone Number input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('')
+  const [phoneNum, setPhoneNum] = useState('')
+
   return (
     <div className="App">
-      Enter your Message
+      <div>
+        <label>
+          Enter your Message
+          <input
+            name='message'
+            onChange={(e) => setMessage(e.target.value)} value={message}/>
+        </label>
+      </div>
+      <div>
+        <label>
+          Enter your Phone Number
+          <input
+            name='phoneNum'
+            onChange={(e) => setPhoneNum(e.target.value)} value={phoneNum}/>
+        </label>
+      </div>
+      <button>Schedule!</button>
     </div>
   );
 }
