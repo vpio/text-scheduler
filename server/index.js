9faaa8c7d61bd6426e30d379562614bb5f3b4120
Pio Molina <vpio@comcast.net>
2019-09-08 13:33:32 -0500

    server successfully manages twilio requests

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const client = require('twilio')(
-  process.env.TWILIO_ACCOUT_SID,
-  process.env.TWILIO_AUTH_TOKEN,
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 );
+console.log(client)
+const PORT = 3001
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+app.post('/api/messages', (req, res) => {
+  res.send(JSON.stringify({ message: req.body.message }))
+  return
+  res.header('Content-Type', 'application/json');
+  client.messages.create({
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: req.body.to,
+    body: req.body.message,
+  })
+  .then(() => {
+    res.send(JSON.stringify({ success: true }));
+  })
+  .catch(err => {
+    console.log('error here', err);
+    res.send(JSON.stringify({ success: false }));
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
