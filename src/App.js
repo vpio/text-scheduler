af54d58a4fa3439e362e90a30b1f00b5f3215044
Pio Molina <vpio@comcast.net>
2019-09-08 20:23:33 -0500

    text scheduler is able to send texts at a scheduled time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
 import axios from 'axios'
+
+function formatForCron(date, time) {
+  let dateInMonth = date.getDate()
+  let month = date.getMonth() + 1
+  let dayOfWeek = date.getDay()
+  let hour = time.hour()
+  let minute = time.minute()
+
+  return `${minute} ${hour} ${dateInMonth} ${month} ${dayOfWeek}`
+}
 
 function App() {
   const [message, setMessage] = useState('')
   const [phoneNum, setPhoneNum] = useState('')
+  const [selectedDate, setDate] = useState(new Date())
+  const [selectedTime, setSelectedTime] = useState(null)
 
   const handleSubmit = () => {
+    let cronFormatted = formatForCron(selectedDate, selectedTime)
+
     axios.post('/api/messages', {
       to: '+1' + phoneNum,
       message: message,
+      selectedTime: cronFormatted,
     })
+  }
+
+  const handleChange = date => {
+    setDate(date)
+  }
+
+  const handleTime = timeMoment => {
+    setSelectedTime(timeMoment)
   }
 
   return (
@@ -33,8 +60,13 @@ function App() {
       <div>
         <label>
           When do you want to receive your message?
-          <input
-            type='date'
+          <DatePicker selected={selectedDate} onChange={handleChange} />
+          <TimePicker
+            showSecond={false}
+            use12Hours={true}
+            defaultValue={null}
+            onChange={handleTime}
+            value={selectedTime}
             />
         </label>
       </div>
